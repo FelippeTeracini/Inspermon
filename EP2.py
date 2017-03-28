eb2ff31e4d04762aaa7083021174bbefd7570542
TeraByte900 <fe.n.teracini@hotmail.com>
2017-03-28 17:33:49 -0300

    Inspermon

--- EP2.py
+++ EP2.py
@@ -1,7 +1,24 @@
-print("oi")
+import random
+while True:
+	lista_de_inspermons:["Hsumon","Diegomon","Hsumon","Teramon"]
+	print("Bem vindo ao Inspermón")
+	nome = input("Qual o nome do seu Inspermón? ")
+	ataque1=5
+	ataque2=1
+	defesa=5
+	vida=20
+	while True:
+		comando = input("Oque você deseja fazer? ")
+		if comando == "dormir":
+			break
+		elif comando == "passear":
+			x=random.randint(0,len(lista_de_inspermons))
+			print("Voce esta batalhando contra {}".format(x))
+			ataque_oponente=2
+			defesa_oponente=2
+			vida_oponente=10
+			
 
-print('Hello, world')
-
-print('Bye, world')
-
-print("Fala HSU!!")
+			continue
+		else:
+			print("Digite um comando válido.")
