e28fa8d9023b98410bd10d21f2c2df87ed9a7425
vinihsu <vinihsu1997@hotmail.com>
2017-03-28 21:08:04 -0300

    nomeação

--- EP2.py
+++ EP2.py
@@ -1,32 +1,50 @@
 import random
+
 while True:
-	lista_de_inspermons=["Hsumon","Diegomon","Hsumon","Teramon"]
-	print("Bem vindo ao Inspermón")
-	nome = input("Qual o nome do seu Inspermón? ")
+
+	lista_de_inspermons=["Hsumon","Diegomon","Hsumon","Teramon"]  # Criação dos Inspermons
+
+	print("Bem vindo ao Inspermón")  # Início do jogo
+
+	nome = input("Qual o nome do seu Inspermón? ")  # Caracterização do próprio personagem
 	ataque1=5
-	ataque2=1
 	defesa=5
 	vida=20
-	while True:
-		comando = input("Oque você deseja fazer? ")
-		if comando == "dormir":
+# Início do jogo:
+	while True:  # Jogo
+		comando = input("Oque você deseja fazer? ")  # Pergunta sobre o que o usuário deseja fazer
+
+		if comando == "dormir":   # Dormir: para o jogo e salva os dados
 			break
-		elif comando == "passear":
-			x=random.randint(0,len(lista_de_inspermons))
-			print("Voce esta batalhando contra {}".format(x))
-			ataque_oponente=2
-			defesa_oponente=2
-			vida_oponente=10
+
+# Inspermon adversário:
+		elif comando == "passear":   # Passear em busca de novos inspermons
+			x=random.randint(0,len(lista_de_inspermons))   # Escolha aleatória do inspermon adversário
+			print("Voce esta batalhando contra {}".format(x))  
+
+			ataque_oponente=random.randint(1,10)  # Definição aleatória do atributos do Inspermon adversário
+			defesa_oponente=random.randint(1,10)
+			vida_oponente=random.randint(10,20)
+
+			print("Este Inspermón possui os seguintes atributos: {} de ataque, {} de defesa e {} de vida" .format (ataque_oponente, defesa_oponente, vida_oponente))
+
+# Batalha:
 			while True:
-				ataque_ativo=int(input("Qual ataque deseja usar? "))
-				if ataque_ativo == 1:
+
+				ataque_ativo=int(input("Qual ataque deseja usar? "))  # Escolha do ataque a ser utilizado
+
+				if ataque_ativo == 1:  # Ataque 1 é o ataque do inspermon igual a seu atributo
+						
 						vida_oponente=vida_oponente-(ataque1-defesa_oponente)
-						if vida_oponente>0:
-							print("{} deu {} de dano no seu oponente, agora ele tem {} de vida".format(nome,ataque1-defesa_oponente,vida_oponente))
+
+						if vida_oponente>0:  # Realização do ataque e atualização da vida restante
+							print("{} deu {} de dano no seu oponente, agora ele tem {} de vida".format(nome,ataque1-defesa_oponente,vida_oponente))  # Cálculo da vida restante
 							continue
+
 						else:
 							print("Voce derrotou esse Inspermon!")
 							break
+
 				elif ataque_ativo == 2:
 					ataque2=random.randint(1,10)
 					vida_oponente=vida_oponente-(ataque2-defesa_oponente)
@@ -36,11 +54,16 @@ while True:
 					else:
 						print("Voce derrotou esse Inspermon!")
 						break
+
 				else:
 					print("Digite um ataque valido!")
 					continue
+
 			continue
+
 		else:
 			print("Digite um comando válido.")
+
 	print("Ate a proxima!")
+
 	continue
