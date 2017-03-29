b931e527154f7e07e559a181f9813a253f74e3f8
Cavi <Cavi>
2017-03-29 11:23:36 -0300

    nomeação do código

--- EP2.py
+++ EP2.py
@@ -30,62 +30,64 @@ while True:
 
 # Batalha:
 			while True:
-				ação=input("O que você deseja fazer? (Atacar ou Fugir): ")
-				if ação == "fugir":
-					chancefugir=random.randint(1,3)
+				ação=input("O que você deseja fazer? (Atacar ou Fugir): ") # Pergunta se deseja atacar ou fugir
+				
+				if ação == "fugir": # Fuga
+					chancefugir=random.randint(1,3)  # Sorte de fuga
 					if chancefugir==3:
-						print("Você conseguiu fuigr")
+						print("Você conseguiu fuigr")  # Fuga com sucesso
 						break
 					else:
-						print("Você não conseguiu fugir")
+						print("Você não conseguiu fugir")  # Fuga sem sucesso
 
-				if ação == "atacar":
-
+				if ação == "atacar": # Ataque
+					ataque_ativo=str
 					ataque_ativo=int(input("Qual ataque deseja usar? "))  # Escolha do ataque a ser utilizado
 
 					if ataque_ativo == 1:  # Ataque 1 é o ataque do inspermon igual a seu atributo
-						if ataque1-defesa_oponente>0:
+						
+						if ataque1-defesa_oponente>0: # Verifica se o inspermon causou dano no adverário
 							vida_oponente=vida_oponente-(ataque1-defesa_oponente)
 
 							if vida_oponente>0:  # Realização do ataque e atualização da vida restante
 								print("{} deu {} de dano em seu oponente, agora ele tem {} de vida".format(nome,ataque1-defesa_oponente,vida_oponente))  # Cálculo da vida restante
 								continue
 
-						if ataque1-defesa_oponente<=0:
+						if ataque1-defesa_oponente<=0:  # Ataque sem causar dano
 							print("{} não deu dano em seu oponente, ele continua com {} de vida".format(nome,vida_oponente))
 							continue
 
-						if vida_oponente<=0:
+						if vida_oponente<=0:  # Fim da batalha, vitória
 							print("Voce derrotou esse Inspermon!")
 							break
 
-					elif ataque_ativo == 2:
+					elif ataque_ativo == 2:  # Ataque 2 valor de ataque aleatório
 						ataque2=random.randint(1,10)
 
-						if ataque2-defesa_oponente>0:
+						if ataque2-defesa_oponente>0:  # Verifica se o inspermon causou dano no adverário
 							vida_oponente=vida_oponente-(ataque2-defesa_oponente)
 
-							if vida_oponente>0:  # Realização do ataque e atualização da vida restante
+							if vida_oponente>0:  # Realização do ataque 2 e atualização da vida restante
 								print("{} deu {} de dano em seu oponente, agora ele tem {} de vida".format(nome,ataque2-defesa_oponente,vida_oponente))  # Cálculo da vida restante
 								continue
 
-						if ataque2-defesa_oponente<=0:
+						if ataque2-defesa_oponente<=0:  # Notificação de não ter dado dano algum
 							print("{} não deu dano em seu oponente, ele continua com {} de vida".format(nome,vida_oponente))
 							continue
 
-						if vida_oponente<=0:
+						if vida_oponente<=0:  # Derrota do oponente
 							print("Voce derrotou esse Inspermon!")
 							break
-					else:
+					else:  # Erro de digitação
 						print("Digite um ataque valido!")
 						continue
 
 			continue
 
-		else:
+		else:  # Erro de digitação
 			print("Digite um comando válido.")
 
 	print("Até a proxima!")
 
 	break
-	
+
