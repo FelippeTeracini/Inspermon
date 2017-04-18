182082994ff2eb956cc268596ef61d395c95b90a
vinihsu <vinihsu1997@hotmail.com>
2017-04-17 21:28:44 -0300

    mudança para classes

---
+++ class inspermons.py
@@ -0,0 +1,20 @@
+import pickle
+class pokemons:
+
+	def __init__(self, ataque, defesa, vida):
+		self.ataque=ataque
+		self.defesa=defesa
+		self.vida=vida
+
+Sharmander=pokemons(5,2,15)
+Skuirtle=pokemons(3,5,10)
+Bulbatauro=pokemons(3,3,20)
+Pikaxu=pokemons(3,2,18)
+Tatata=pokemons(5,2,12)
+Pigey=pokemons(2,1,15)
+Dogerpie=pokemons(3,4,10)
+Magipark=pokemons(1,1,20)
+
+
+#with open("class_inspermons.pickle","wb") as arquivo_class_inpermons:
+#pickle.dump(class_inspermons,arquivo_class_inpermons)
