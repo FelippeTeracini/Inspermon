e001f6e8dd5ed238ad4b38300865bf507f528a77
vinihsu <vinihsu1997@hotmail.com>
2017-04-18 20:36:59 -0300

    tentativa json

---
+++ pokemon_dicionario.py
@@ -0,0 +1,17 @@
+import json
+
+class Pokemon:
+	def __init__(self, nome, ataque, defesa, vida):
+		self.nome = nome
+		self.ataque = ataque
+		self.defesa = defesa
+		self.vida = vida
+
+with open("pokemons.json") as json_data:
+	pokemons = json.load(json_data)
+
+pokemonsDict = dict
+
+for p in pokemons:
+	pokemonsDict[p["pokemons"].nome] = Pokemon(p["pokemons"][p].nome, [p].ataque , [p].defesa , [p].vida) 
+	print(p)
