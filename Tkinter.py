22f46cecf39710e7fdd7bedb18edc127cd6ac414
vinihsu <vinihsu1997@hotmail.com>
2017-04-18 21:59:29 -0300

    desenho da interface inicial

--- Tkinter.py
+++ Tkinter.py
@@ -1,12 +1,12 @@
 from tkinter import *
 
 root=Tk()	
-
 menu=Menu(root)
 root.config(menu=menu)
+#root.minsize(width=1360, height=720)
 root.wm_title("Inspérmon")
-#root.wm_pack(side=LEFT)
 
+#     ===================  Toolbar  ==================
 arquivo=Menu(menu)
 menu.add_cascade(label="Arquivo", menu=arquivo)
 arquivo.add_command(label="Salvar")#, command=)
@@ -15,6 +15,29 @@ arquivo.add_command(label="Abrir")#, command=)
 ajuda=Menu(menu)
 menu.add_command(label="Ajuda")#, command=open.ajuda)
 
+#    ======================= Interface Inicial =======================
+#botão do iniciais
+pokemon_inicial = Label(root, text="Escolha seu Ispérmon inicial")
+pokemon_inicial.grid(row=0, columnspan=3)
+sharmander = Button(root, text="Sharmander")#, command=)
+sharmander.grid(row=3)
+bulbatauro = Button(root, text="Bulbatauro")#, command=)
+bulbatauro.grid(row=3,column=1)
+skuirtle = Button(root, text="Skuirtle")#, command=)
+skuirtle.grid(row=3,column=2)
+
+# fotos dos iniciais
+fotosharmander = PhotoImage(file="")
+sharmanderlabel = Label(root, image=fotosharmander)
+sharmanderlabel.grid(row=1)
+
+fotobulbatauro = PhotoImage(file="")
+bulbataurolabel = Label(root, image=fotobulbatauro)
+bulbataurolabel.grid(row=1,column=1)
+
+fotoskuirtle = PhotoImage(file="")
+skuirtlelabel = Label(root, image=fotoskuirtle)
+skuirtlelabel.grid(row=1,column=2)
+
+
 root.mainloop()
-
-
